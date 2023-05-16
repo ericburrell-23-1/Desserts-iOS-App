42f6e399303412876590b82c3cc3977a5d430b8a
Eric Burrell <ericburrell231@gmail.com>
2023-05-16 14:43:24 -0500

    HttpClient fetch function built and tests added

--- Desserts/Utility/HTTPClient.swift
+++ Desserts/Utility/HTTPClient.swift
@@ -8,20 +8,26 @@
 import Foundation
 
 class HttpClient {
+    let urlSession: URLSession
+    
+    init(urlSession: URLSession = URLSession.shared) {
+        self.urlSession = urlSession
+    }
+    
     func fetch<T: Codable>(urlString: String) async throws -> [T] {
         guard let url = URL(string: urlString) else {
             throw HttpError.badURL()
         }
-        let (data, response) = try await URLSession.shared.data(from: url)
+        let (data, response) = try await urlSession.data(from: url)
         
         guard (response as? HTTPURLResponse)?.statusCode == 200 else {
                    throw HttpError.badResponse()
                }
         
-        guard let object = try? JSONDecoder().decode([T].self, from: data) else {
+        guard let object = try? JSONDecoder().decode(Dictionary<String, [T]>.self, from: data) else {
             throw HttpError.errorDecodingData()
         }
         
-        return object
+        return object.values.first ?? []
     }
 }
--- DessertsTests/HTTPClientTests.swift
+++ DessertsTests/HTTPClientTests.swift
@@ -9,17 +9,118 @@ import XCTest
 @testable import Desserts
 
 final class HTTPClientTests: XCTestCase {
+    var httpClient: HttpClient!
+    var expectation: XCTestExpectation!
+    var apiURLString = "https://jsonplaceholder.typicode.com/posts/42"
+    var strMeal: String = "Dessert Name"
+    var strMealThumb: String = "https://www.example.com"
+    var idMeal: String = "12345"
+    var httpStatusCode: Int = 200
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        // Prepare mock response for success case
+        strMeal = "Dessert Name"
+        strMealThumb = "https://www.example.com"
+        idMeal = "12345"
+        httpStatusCode = 200
+        
+        let configuration = URLSessionConfiguration.default
+        configuration.protocolClasses = [MockURLProtocol.self]
+        let urlSession = URLSession.init(configuration: configuration)
+        
+        httpClient = HttpClient(urlSession: urlSession)
+        expectation = expectation(description: "Expectation")
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testHTTPClientThrowsBadURLError() {
-        // Mock the HTTPClient URLSession, set up dependency injection, test all client outcomes
+    
+    func prepareMockRequest() {
+        let jsonString = """
+                            {
+                                "meals": [
+                                    {
+                                        "strMeal": "\(strMeal)",
+                                        "strMealThumb": "\(strMealThumb)",
+                                        "idMeal": "\(idMeal)"
+                                    }
+                                ]
+                            }
+                         """
+         let data = jsonString.data(using: .utf8)
+    
+        // Set up mock request handler
+        MockURLProtocol.requestHandler = { request in
+            guard let url = request.url, url == URL(string: self.apiURLString) else {
+                throw HttpError.badURL("Test URL is invalid")
+            }
+            
+            let response = HTTPURLResponse(url: url, statusCode: self.httpStatusCode, httpVersion: nil, headerFields: nil)!
+            return (response, data)
+        }
         
+    }
+    func testSuccessfulResponse() async {
+        
+        prepareMockRequest()
+        
+        do {
+            let result: [meal] = try await httpClient.fetch(urlString: apiURLString)
+            XCTAssertEqual(result[0].strMeal, strMeal)
+            XCTAssertEqual(result[0].strMealThumb, strMealThumb)
+            XCTAssertEqual(result[0].idMeal, idMeal)
+            self.expectation.fulfill()
+        } catch {
+            XCTFail("Unexpectedly returned error: \(error)")
+            self.expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 1.0)
+        
+    }
+    
+    func testBadURLThrowsError() async {
+        apiURLString = "http://abc.com/%"
+        prepareMockRequest()
+        
+        do {
+            let _: [meal]  = try await httpClient.fetch(urlString: apiURLString)
+            XCTFail("Should have thrown badURL error")
+            self.expectation.fulfill()
+        } catch {
+            XCTAssertEqual(error.localizedDescription, HttpError.badURL().localizedDescription)
+            self.expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 1.0)
+    }
+    
+    func testBadResponseThrowsError() async {
+        httpStatusCode = 400
+        prepareMockRequest()
+        
+        do {
+            let _: [meal]  = try await httpClient.fetch(urlString: apiURLString)
+            XCTFail("Should have thrown badResponse error")
+            self.expectation.fulfill()
+        } catch {
+            XCTAssertEqual(error.localizedDescription, HttpError.badResponse().localizedDescription)
+            self.expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 1.0)
+    }
+    
+    func testCannotDecodeJSONThrowsError() async {
+        strMeal = "\""
+        prepareMockRequest()
+        
+        do {
+            let _: [meal]  = try await httpClient.fetch(urlString: apiURLString)
+            XCTFail("Should have thrown errorDecodingData error")
+            self.expectation.fulfill()
+        } catch {
+            XCTAssertEqual(error.localizedDescription, HttpError.errorDecodingData().localizedDescription)
+            self.expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 1.0)
     }
 }
